973e0cc6371960c731c9e4143c00f9dfb2c5921b
YJ Yang <chcokr@gmail.com>
2015-08-02 16:18:14 -0400

    <StylesheetHotLoad>

---
+++ StylesheetHotLoad.jsx
@@ -0,0 +1,47 @@
+import autoprefixer from 'autoprefixer-core';
+import postcss from 'postcss';
+import React from 'react';
+import {Router, Route, Link, Navigation, TransitionHook} from 'react-router';
+import stilr from 'stilr';
+
+function getStilrCSSObj() {
+  return {
+    stilrCSS: postcss(autoprefixer()).process(stilr.render()).css
+  };
+}
+
+const StylesheetHotLoad = React.createClass({
+  displayName: 'StylesheetHotLoad',
+  mixins: [Navigation, TransitionHook],
+  getInitialState() {
+    if (process.env.NODE_ENV !== 'production') {
+      return {
+        stilrCSS: ''
+      };
+    }
+  },
+  routerWillLeave() {
+    if (process.env.NODE_ENV !== 'production') {
+      // TODO: I couldn't figure out how to get the stilr CSS object after the
+      // transition is done, so I'm doing a hacky setTimeout here
+      setTimeout(() => {
+        this.setState(getStilrCSSObj());
+      }, 50);
+    }
+  },
+  componentDidMount() {
+    if (process.env.NODE_ENV !== 'production') {
+      this.setState(getStilrCSSObj());
+    }
+  },
+  render() {
+    return (
+      <div>
+        <style>
+          {process.env.NODE_ENV !== 'production' ? this.state.stilrCSS : ''}
+        </style>
+        {this.props.children}
+      </div>
+    );
+  }
+});
