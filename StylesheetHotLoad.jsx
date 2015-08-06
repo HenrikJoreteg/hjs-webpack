5e38478faa9f6f2eace3c5de17654521473b73dc
YJ Yang <chcokr@gmail.com>
2015-08-06 08:42:41 -0400

    Temp

--- StylesheetHotLoad.jsx
+++ StylesheetHotLoad.jsx
@@ -2,17 +2,11 @@ import React from 'react';
 import {Navigation, TransitionHook} from 'react-router';
 
 function getStilrCSSObj() {
-  if (process.env.NODE_ENV !== 'production') {
-    const autoprefixer = require('autoprefixer-core');
-    const postcss = require('postcss');
-    const stilr = require('stilr');
-    return {
-      stilrCSS: postcss(autoprefixer()).process(stilr.render()).css
-    };
-  }
-
+  const autoprefixer = require('autoprefixer-core');
+  const postcss = require('postcss');
+  const stilr = require('stilr');
   return {
-    stilrCSS: "This shouldn't show up in production!"
+    stilrCSS: postcss(autoprefixer()).process(stilr.render()).css
   };
 }
 
@@ -20,31 +14,25 @@ const StylesheetHotLoad = React.createClass({
   displayName: 'StylesheetHotLoad',
   mixins: [Navigation, TransitionHook],
   getInitialState() {
-    if (process.env.NODE_ENV !== 'production') {
-      return {
-        stilrCSS: ''
-      };
+    return {
+      stilrCSS: ''
     }
   },
   routerWillLeave() {
-    if (process.env.NODE_ENV !== 'production') {
-      // TODO: I couldn't figure out how to get the stilr CSS object after the
-      // transition is done, so I'm doing a hacky setTimeout here
-      setTimeout(() => {
-        this.setState(getStilrCSSObj());
-      }, 50);
-    }
+    // TODO: I couldn't figure out how to get the stilr CSS object after the
+    // transition is done, so I'm doing a hacky setTimeout here
+    setTimeout(() => {
+      this.setState(getStilrCSSObj());
+    }, 50);
   },
   componentDidMount() {
-    if (process.env.NODE_ENV !== 'production') {
-      this.setState(getStilrCSSObj());
-    }
+    this.setState(getStilrCSSObj());
   },
   render() {
     return (
       <div>
         <style>
-          {process.env.NODE_ENV !== 'production' ? this.state.stilrCSS : ''}
+          this.state.stilrCSS
         </style>
         {this.props.children}
       </div>
