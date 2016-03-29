2f3a52f64abab365af4e4216e472d3bae44e711e
René van Mil <rvanmil@me.com>
2016-03-29 21:55:18 +0200

    Add support for http-proxy-middleware

--- bin/hjs-dev-server.js
+++ bin/hjs-dev-server.js
@@ -8,6 +8,7 @@ var path = require('path')
 var express = require('express')
 var webpack = require('webpack')
 var assign = require('lodash.assign')
+var httpProxyMiddleware = require("http-proxy-middleware");
 
 var configFile = process.argv[2] || 'webpack.config.js'
 var config
@@ -46,6 +47,18 @@ if (https) {
 
 var compiler = webpack(config)
 
+if (serverConfig.proxy) {
+  if (!Array.isArray(serverConfig.proxy)) {
+    serverConfig.proxy = [serverConfig.proxy]
+  }
+  serverConfig.proxy.forEach(function(proxyConfig) {
+    var proxy = proxyMiddleware(proxyConfig.context, proxyConfig.options)
+    app.use(function(req, res, next) {
+      next();
+    }, proxy);
+  });
+}
+
 if (serverConfig.historyApiFallback) {
   app.use(require('connect-history-api-fallback')({
     verbose: false
@@ -62,7 +75,7 @@ if (serverConfig.contentBase) {
   app.use(express.static(serverConfig.contentBase))
 }
 
-server.listen(serverConfig.port, serverConfig.hostname, function (err) {
+server.listen(serverConfig.port, serverConfig.hostname, function(err) {
   if (err) {
     console.error(err)
     return
