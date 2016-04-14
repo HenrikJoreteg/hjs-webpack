ff9715126bd063bb59368657c938cfc841b5c823
Luke Karrys <lukekarrys@gmail.com>
2016-04-14 10:50:38 -0700

    fix ssr and hmre in prerender example; closes #205

--- examples/prerendered-html-files/webpack.config.js
+++ examples/prerendered-html-files/webpack.config.js
@@ -2,6 +2,7 @@
 require('babel-core/register')
 
 // require React and our two React components
+var fs = require('fs')
 var React = require('react')
 var ReactDOMServer = require('react-dom/server')
 var HomePage = require('./src/home-page').default
@@ -10,7 +11,7 @@ var OtherPage = require('./src/other-page').default
 // our hjs-webpack, of course
 var getConfig = require('hjs-webpack')
 
-module.exports = getConfig({
+var config = getConfig({
   in: 'src/app.js',
   out: 'public',
   clearBeforeBuild: true,
@@ -21,8 +22,8 @@ module.exports = getConfig({
     var otherPageHtmlString = ReactDOMServer.renderToString(React.createElement(OtherPage))
 
     return {
-      'index.html': data.defaultTemplate({html: homePageHtmlString}),
-      'other/index.html': data.defaultTemplate({html: otherPageHtmlString})
+      'index.html': data.defaultTemplate({html: `<div id='root'>${homePageHtmlString}</div>`}),
+      'other/index.html': data.defaultTemplate({html: `<div id='root'>${otherPageHtmlString}</div>`})
 
       // here we might also create an app manifest.json
       // app-manifest.json: JSON.stringify(something)
@@ -32,3 +33,11 @@ module.exports = getConfig({
     }
   }
 })
+
+// Having hmre present in the .babelrc will break with the `babel-core/register` above
+// so wait until that is done and then add it here via the loader query
+const babelrc = JSON.parse(fs.readFileSync('./.babelrc'))
+babelrc.env = {development: {presets: ['react-hmre']}}
+config.module.loaders[0].query = babelrc
+
+module.exports = config
